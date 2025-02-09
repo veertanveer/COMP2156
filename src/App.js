04ac480292da4d91d0c5fe64b82d6f8000972bfb
Veer Tanveer <veertanveer@georgebrown.ca>
2025-02-09 13:32:47 -0500

    Including application source

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React, { useState } from "react";
+import StudentForm from "./components/StudentForm";
+import StudentList from "./components/StudentList";
+
+const App = () => {
+  const [students, setStudents] = useState([]);
+
+  const addStudent = (student) => {
+    setStudents([...students, student]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Student ID: 101486671</h1>
+      <h1>Student Name: Veer Tanveer</h1>
+      <h1>Student Information System</h1>
+      <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
+      <StudentForm addStudent={addStudent} />
+      Developerd By: Veer Tanveer
+      <StudentList students={students} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/StudentList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StudentList = () => {
+  return (
+    <div>
+      <h2>Student List</h2>
+    </div>
+  );
+};
+
+export default StudentList;
